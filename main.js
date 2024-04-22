5ce3a0db6ceaa4568a9b05f17037ed17ac431d54
sarahnizer <136729012+sarahnizer@users.noreply.github.com>
2024-04-22 12:18:40 -0300

    kkk

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador")
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00")
-const tempoObjetivo2 = new Date("2024-10-05T00:00:00")
-const tempoObjetivo3 = new Date("2024-10-05T00:00:00")
-const tempoObjetivo4 = new Date("2024-10-05T00:00:00")
+const tempoObjetivo1 = new Date("1995-11-18T00:00:00")
+const tempoObjetivo2 = new Date("2024-06-11T:00:00")
+const tempoObjetivo3 = new Date("2024-10-16T00:00:00")
+const tempoObjetivo4 = new Date("2024-11-23T00:00:00")
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 contadores[0].textContent = calculaTempo(tempos[0]);
@@ -40,8 +40,7 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000); }
     comecaCronometro();
 
-atualizaCronometro();
-setInterval(atualizaCronometro, 1000);
+
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
